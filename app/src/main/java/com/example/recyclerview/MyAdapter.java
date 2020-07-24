0416c1997f0ebb4630cbf5134a60d98d8f3235da
mahuanfy <mahuanfy@foxmail.com>
2020-07-24 12:15:24 +0800

    [Huanhuan]: refactor the code.

--- app/src/main/java/com/example/recyclerview/MyAdapter.java
+++ app/src/main/java/com/example/recyclerview/MyAdapter.java
@@ -11,16 +11,21 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.RequestManager;
 import com.example.recyclerview.data.ItemData;
+
+import java.util.List;
 
 import static com.example.recyclerview.data.TypeConstant.TYPE_HEADER;
 import static com.example.recyclerview.data.TypeConstant.TYPE_ITEM;
 
 public class MyAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
-    public Context context;
+    private List<ItemData> itemDataList;
+    private RequestManager requestManager;
 
-    public MyAdapter(Context context) {
-        this.context = context;
+    public MyAdapter(Context context, List<ItemData> itemDataList) {
+        this.itemDataList = itemDataList;
+        this.requestManager = Glide.with(context);
     }
 
     @NonNull
@@ -40,7 +45,7 @@ public class MyAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
     @Override
     public int getItemViewType(int position) {
-        ItemData itemData = ItemData.initItemData().get(position);
+        ItemData itemData = itemDataList.get(position);
         switch (itemData.type) {
             case 0:
                 return TYPE_ITEM;
@@ -53,34 +58,46 @@ public class MyAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
     @Override
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
-        ItemData itemData = ItemData.initItemData().get(position);
+        ItemData itemData = itemDataList.get(position);
         switch (itemData.type) {
             case TYPE_HEADER:
-                ((HeaderViewHolder) holder).headerContent.setText(itemData.title);
+                HeaderViewHolder headerViewHolder = (HeaderViewHolder) holder;
+                headerViewHolder.setHeaderContentText(itemData.title);
                 break;
             case TYPE_ITEM:
-                Glide.with(context).load(itemData.avatar).into(((TextViewHolder) holder).imageUrl);
-                ((TextViewHolder) holder).title.setText(itemData.title);
-                ((TextViewHolder) holder).number.setText(String.valueOf(itemData.number));
-                ((TextViewHolder) holder).description.setText(itemData.description);
-
+                TextViewHolder textViewHolder = (TextViewHolder) holder;
+                requestManager.load(itemData.avatar).into(textViewHolder.imageUrl);
+                textViewHolder.setTitleText(itemData.title);
+                textViewHolder.setNumberText(String.valueOf(itemData.number));
+                textViewHolder.setDescriptionText(itemData.description);
                 break;
         }
     }
 
     @Override
     public int getItemCount() {
-        return ItemData.initItemData().size();
+        return itemDataList.size();
     }
 
     public static class TextViewHolder extends RecyclerView.ViewHolder {
-        public View itemView;
         public TextView title, number, description;
         public ImageView imageUrl;
 
+        public void setTitleText(String title) {
+            this.title.setText(title);
+        }
+
+        public void setNumberText(String number) {
+            this.number.setText(number);
+        }
+
+        public void setDescriptionText(String  description) {
+            this.description.setText(description);
+        }
+
+
         public TextViewHolder(@NonNull View itemView) {
             super(itemView);
-            this.itemView = itemView;
             this.title = itemView.findViewById(R.id.item_title);
             this.number = itemView.findViewById(R.id.item_number);
             this.description = itemView.findViewById(R.id.item_description);
@@ -89,12 +106,14 @@ public class MyAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
     }
 
     public static class HeaderViewHolder extends RecyclerView.ViewHolder {
-        public View itemView;
         public TextView headerContent;
+
+        public void setHeaderContentText(String headerContent) {
+            this.headerContent.setText(headerContent);
+        }
 
         public HeaderViewHolder(@NonNull View itemView) {
             super(itemView);
-            this.itemView = itemView;
             this.headerContent = itemView.findViewById(R.id.header_content);
         }
     }
